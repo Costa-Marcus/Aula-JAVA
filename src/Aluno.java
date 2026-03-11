4f2de1b17736fc634dfc115350e26b518cd7ad18
Costa-Marcus <labsfiap@fiap.com.br>
2026-03-11 20:52:12 -0300

    apostila 03

---
+++ src/Aluno.java
@@ -0,0 +1,28 @@
+public class Aluno {
+
+short idade;
+// definindo os atributos do aluno
+
+int id;
+
+char turma;
+
+String nomeAluno;
+
+String nomeCurso;
+
+boolean matriculado;
+
+double valorMensalidade;
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Master.java
@@ -0,0 +1,22 @@
+public class Master {
+    public static void main(String[] args) {
+System.out.println("Bom Dia!");
+
+// declarar variaves no java
+        short idade;
+        //atribuir idade para variavel(valor)
+ idade = 32;
+ //declarar a variavel e atribuir um valor
+ int anoNascimento = 1994;
+ double salario = 16000.56;
+ float altura = 16000.50f;
+ char inicialNome =  'L';
+ boolean maiorIdade = true; //false
+         String nome = "Lucas";
+
+
+
+
+
+    }
+}
